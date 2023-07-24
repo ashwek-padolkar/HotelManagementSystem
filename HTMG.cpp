91bf25eb045e7cdf13def27c8c430c3b60c4179f
AshwekPadolkar <131592430+ashwek-padolkar@users.noreply.github.com>
2023-07-25 01:44:29 +0530

    Update HTMG.cpp

--- HTMG.cpp
+++ HTMG.cpp
@@ -1,6 +1,11 @@
-// Main Code (You make changes in Hm.cpp code and paste the changes in this code)
-// Basic Functions of this code works but this code is  not fully optimized, it is still under repair.
-
+/*
+    count :- keeps track of the number of rooms added by the hotel manager.
+    ci_count :- keeps track of the number of customers checked-in.
+    ci_status :- 0: indicates that the customer has either checked-out or not yet checked-in, 1: checked-in successfully.
+    ci_flag :- in checkInRoom(), 0: room no. does not exists, 1: exists.
+    search_flag :- in searchCustomer(), 0: person not found, 1: person found.
+    co_flag :- in checkOutRoom(), 0: room no. does not exists OR it is not reserved, 1: checked-out successfully.
+*/
 #include<iostream>
 #include<string.h>
 #include<math.h>
@@ -10,97 +15,135 @@ class Room
 {
 private:
 
-    //addRooms()
-    int room_no[50];
-    float room_rent[50];
+    // addRooms()
+    int room_no[50],room_rent[50];
     string room_btype[50],room_stype[50],room_ac[50];
 
-    //checkInRoom()
-    int ci_room_no,ci_count=0,ci_status[50];
-    float advance_payment[50];
+    // checkInRoom()
+    int ci_room_no,advance_payment[50],ci_count=0,ci_status[50];
     string cust_name[50],cust_address[50];
     string ci_date[50],co_date[50];
 
 public:
-    int count=0,i=0;                      // count = no. of Add Rooms, i = index of room no.
+    int count=0;
 
     void addRooms()
     {
-        cout<<"Enter Room No.: ";
-        cin>>room_no[i];
-        cout<<"\nType AC/Non-AC (A/N) : ";
-        cin>>room_ac[i];
-        cout<<"\nType Beds Single/Double (S/D) : ";
-        cin>>room_btype[i];
-        cout<<"\nType Size (B/S) : ";
-        cin>>room_stype[i];
-        cout<<"\nDaily Rent : ";
-        cin>>room_rent[i];
-        cout<<"\n Room Added Successfully!\n";
+        if (count>=50)
+        {
+            cout<<"Cannot add more rooms. Maximum limit reached."<<endl;
+            return;
+        }
 
-        ci_status[i]=0;         // sets check-in status=0 means customer has not yet checked-in.
+        int new_room_no;
+        bool room_exists=false;
 
-        count++;
-        i++;
+        do
+        {
+            cout<<"Enter Room No.: ";
+            cin >> new_room_no;
+
+            for(int i=0;i<count;i++)
+            {
+                if(room_no[i]==new_room_no)
+                {
+                    room_exists=true;
+                    break;
+                }
+            }
+
+            if(room_exists)
+            {
+                cout<<"Room number "<<new_room_no<<" already exists. Please enter a different room number."<<endl;
+                room_exists=false;
+            }
+            else
+            {
+                room_no[count]=new_room_no;
+
+                cout<<"\nType AC/Non-AC (A/N) : ";
+                cin>>room_ac[count];
+                cout<<"\nType Beds Single/Double (S/D) : ";
+                cin>>room_btype[count];
+                cout<<"\nType Size (B/S) : ";
+                cin>>room_stype[count];
+                cout<<"\nDaily Rent : ";
+                cin>>room_rent[count];
+
+                ci_status[count]=0;
+
+                count++;
+                cout<<"\nRoom Added Successfully!\n";
+            }
+
+        } while(room_exists);
     }
 
     void checkInRoom()
     {
-        int ci_flag=0;          // sets check-in flag = 0
+        int ci_flag=0;
 
-        if(count==0)            // if rooms are not added then
+        if(count==0)                                    // if rooms are not added then
         {
             cout<<"Please add the rooms first.";
             return;
         }
-        else                    // if rooms are added then
+        else                                            // if rooms are added then
         {
             cout<<"\nEnter Room number : ";
             cin>>ci_room_no;
 
             for(int i=0;i<count;i++)
             {
-                if(ci_room_no==room_no[i])     //if the room you want to check-in == room you have already added
+                if(ci_room_no==room_no[i])              //if the room you want to check-in == room you have already added
                 {
-                    cout<<"\nEnter Customer Name (First Name): ";
-                    cin>>cust_name[i];
-                    cout<<"\nEnter Address (only city): ";
-                    cin>>cust_address[i];
-
-                    double digits;
-                    cout<<"\nPLease enter your phone number :\n";
-                    cin>>digits;
-
-                    try                        // Verify if phone number has 10 digits or not using exception handling
+                    if(ci_status[i]==0)
                     {
-                        while(int(log10(digits))<=8||int(log10(digits))>=10)
-                        {
-                            throw(digits);
-                        }
-                        cout<<"\n\t\t\tPhone number is valid ";
-                    }
-                    catch(double digits)
-                    {
-                        cout<<"\n\t\t\tPhone number is not valid. Try Again!!!";
+                        cout<<"\nEnter Customer Name (First Name): ";
+                        cin>>cust_name[i];
+                        cout<<"\nEnter Address (only city): ";
+                        cin>>cust_address[i];
+
+                        double digits;
                         cout<<"\nPLease enter your phone number :\n";
                         cin>>digits;
-                    }
-                    cout<<"\nEnter Check in Date: ";
-                    cin>>ci_date[i];
-                    cout<<"\nEnter Check out Date: ";
-                    cin>>co_date[i];
-                    cout<<"\nEnter Advance Payment: ";
-                    cin>>advance_payment[i];
-                    cout<<"\n Customer Checked-in Successfully...\n";
 
-                    ci_status[i]=1;          // indicates that the customer has checked-in successfully
-                    ci_flag=1;               // sets check-in flag = 1
-                    ci_count++;              // increments the number of customers checked-in
+                        try                        // Verify if phone number has 10 digits or not using exception handling
+                        {
+                            while(int(log10(digits))<=8||int(log10(digits))>=10)
+                            {
+                                throw(digits);
+                            }
+                            cout<<"\n\t\t\tPhone number is valid ";
+                        }
+                        catch(double digits)
+                        {
+                            cout<<"\n\t\t\tPhone number is not valid. Try Again!!!";
+                            cout<<"\nPLease enter your phone number :\n";
+                            cin>>digits;
+                        }
+                        cout<<"\nEnter Check in Date: ";
+                        cin>>ci_date[i];
+                        cout<<"\nEnter Check out Date: ";
+                        cin>>co_date[i];
+                        cout<<"\nEnter Advance Payment: ";
+                        cin>>advance_payment[i];
+                        cout<<"\n Customer Checked-in Successfully...\n";
+
+                        ci_status[i]=1;
+                        ci_flag=1;
+                        ci_count++;
+                    }
+                    else
+                    {
+                        cout<<"Room Number "<<ci_room_no<<" is already occupied!";
+                        return;
+                    }
                 }
             }
-            if(ci_flag==0)                // if check-in flag = 0 then the room you want to check-in is not present in the added rooms
+            if(ci_flag==0)      // if check-in flag = 0 then the room you want to check-in is not present in the added rooms
             {
-                cout<<"Room Number "<<ci_room_no<<" not found! Please add the room first.";
+                cout<<"Room Number "<<ci_room_no<<" does not exists! Please add the room first.";
                 return;
             }
         }
@@ -108,12 +151,12 @@ public:
 
     void getAvailRoom()
     {
-        if(count==0)           // if rooms are not added then
+        if(count==0)                // if rooms are not added then
         {
             cout<<"Please add the rooms first.";
             return;
         }
-        else if(count==ci_count)     // if the number of rooms added == number of customers checked in (means all rooms are reserved)
+        else if(count==ci_count)    // if the number of rooms added == number of customers checked in (means all rooms are reserved)
         {
             cout<<"All rooms are reserved! Please add more rooms.";
             return;
@@ -122,7 +165,7 @@ public:
         {
             for(int i=0;i<count;i++)
             {
-                if(ci_status[i]==0)        // if the rooms are added but have not yet checked-in or when the customers have checked-out after checking in (means the room is available)
+                if(ci_status[i]==0)  // if the rooms are added but have not yet checked-in or when the customers have checked-out after checking in (means the room is available)
                 {
                     cout<<"\n\nRoom No.: "<<room_no[i];
                     cout<<"\nType AC/Non-AC (A/N) : "<<room_ac[i];
@@ -136,16 +179,16 @@ public:
 
     void searchCustomer()
     {
-        int search_flag=0;      // sets the search_flag = 0
+        int search_flag=0;
         string cname;
 
-        if(count==0)            // if rooms are not added then
+        if(count==0)              // if rooms are not added then
         {
             cout<<"Please add the rooms first.";
             return;
         }
 
-        else if(ci_count==0)    // if rooms are added but the customers have not yet checked-in
+        else if(ci_count==0)     // if rooms are added but the customers have not yet checked-in
         {
             cout<<"\n No Guests in Hotel !!";
             return;
@@ -161,10 +204,10 @@ public:
                 {
                     cout<<"\nCustomer Name: "<<cust_name[i];
                     cout<<"\nRoom Number: "<<room_no[i];
-                    search_flag=1;                            // sets the search_flag = 1
+                    search_flag=1;
                 }
             }
-            if(search_flag==0)                  // if the person you are searching for is not present in our hotel
+            if(search_flag==0)         // if the person you are searching for is not present in our hotel
             {
                 cout<<"\nPerson not found.\n";
                 return;
@@ -174,25 +217,27 @@ public:
 
     void checkOutRoom()
     {
-        int days,co_flag=0,co_room_no;       // sets the check-out_flag = 0
-        float billAmount=0;
+        int days,billAmount=0,co_flag=0,co_room_no;
 
-        if(count==0)                         // if rooms are not added then
+        if(count==0)                               // if rooms are not added then
         {
             cout<<"Please add the rooms first.";
+            return;
+        }
+        if(ci_count==0)
+        {
+            cout<<"\n No Guest in Hotel !!";
             return;
         }
         else
         {
             cout<<"Enter Room Number : ";
             cin>>co_room_no;
+
             for(int i=0;i<count;i++)
             {
-                if(co_room_no==room_no[i])
+                if((co_room_no==room_no[i]) && (ci_status[i]==1))
                 {
-                    cout<<"\nEnter Number of Days:\t";
-                    cin>>days;
-                    billAmount=days*room_rent[i];
                     cout<<"\n\t######## CheckOut Details ########\n";
                     cout<<"\nCustomer Name : "<<cust_name[i];
                     cout<<"\nRoom Number : "<<room_no[i];
@@ -201,14 +246,15 @@ public:
                     cout<<"\nAdvance Paid: "<<advance_payment[i]<<"/-";
                     cout<<"\n*** Total Payable: "<<(billAmount-advance_payment[i])<<"/- only";
 
-                    ci_status[i]=0;           // sets check-in status = 0 to make sure that after customer has checked-out, the check-in status of that room remains same as before checking in
-                    ci_count--;               //
-                    co_flag=1;                // sets the check-out_flag = 1
+                    ci_status[i]=0;      // sets check-in status = 0 to make sure that after customer has checked-out, the check-in status of that room remains same as before checking in
+                    co_flag=1;
+                    ci_count--;
                 }
             }
+
             if(co_flag==0)
             {
-                cout<<"Room Number "<<co_room_no<<" not found!";
+                cout<<"Room Number "<<co_room_no<<" does not exists OR it is not reserved!";
                 return;
             }
         }
@@ -216,14 +262,14 @@ public:
 
     void guestSummaryReport()
     {
-        if(count==0)            // if rooms are not added then
+        if(count==0)                    // if rooms are not added then
         {
             cout<<"Please add the rooms first.";
             return;
         }
         else
         {
-            if(ci_count==0)    // if rooms are added but the customers have not yet checked-in
+            if(ci_count==0)                    // if rooms are added but the customers have not yet checked-in
             {
                 cout<<"\n No Guest in Hotel !!";
                 return;
@@ -232,7 +278,7 @@ public:
             {
                 for(int i=0;i<ci_count;i++)
                 {
-                    if(ci_status[i]==1)    // if customers have checked-in
+                    if(ci_status[i]==1)        // if customers have checked-in
                     {
                         cout<<"\n Customer Name : "<<cust_name[i];
                         cout<<"\n Room Number : "<<room_no[i];
